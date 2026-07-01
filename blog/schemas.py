eeef63dee7e7599b40b42dfbcf07b08ce2df4cfb
gnieto <gnieto@bordeaux-inp.fr>
2026-07-01 18:31:54 +0200

    crud operations and user mngt

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,27 @@
 from pydantic import BaseModel
 
+
 class Blog(BaseModel):
-    title: str
+    title: str = "this is a title"
     body: str = "this is a body"
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str        
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    class Config():
+        from_attributes = True
+
+
+class ShowBlog(Blog):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        from_attributes = True
